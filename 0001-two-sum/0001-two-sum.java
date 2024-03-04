dfa6e9af97ca5d64a25514d448564dd1ee39044f
Parth Shah <37403568+parthshah312@users.noreply.github.com>
2024-03-04 13:39:55 -0500

    Time: 31 ms (53.43%), Space: 45.4 MB (6.25%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] outp=new int[2];
+        for(int i=0;i<nums.length-1;i++){
+            int diff=target-nums[i];
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]==diff)
+                {
+                    outp[0]=i;
+                    outp[1]=j;
+                    return outp;
+                }
+            }
+        }
+        return outp;
+    }
+}
